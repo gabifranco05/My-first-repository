d18ca408a48a29fcd877e33d33dabef36572483e
Gabriel Franco <gabriel.f.franco@tecnico.ulisboa.pt>
2023-12-10 17:46:53 +0000

    métodos check

--- Brownian_Motion.py
+++ Brownian_Motion.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 import random
-from math import sqrt, pi
+from math import sqrt, pi, sin, cos, atan, atan2
 
 pygame.init()
 
@@ -17,27 +17,56 @@ WHITE = (255, 255, 255)
 RED = (255, 0 , 0)
 BLUE = (0, 0, 255)
 
+def elastic_collision(v1i, v2i, theta1, theta2):
+    # Calculate relative velocity components
+    v_rel_x = v1i * cos(theta1) - v2i * cos(theta2)
+    v_rel_y = v1i * sin(theta1) - v2i * sin(theta2)
+
+    # Calculate final velocities after collision
+    v1f = v2i + 2 * (v_rel_x * cos(theta2) + v_rel_y * sin(theta2))
+    v2f = v1i + 2 * (v_rel_x * cos(theta1) + v_rel_y * sin(theta1))
+
+    # Calculate final angles after collision
+    phi1 = atan2(v1f * sin(theta1) + v2f * sin(theta2), v1f * cos(theta1) + v2f * cos(theta2))
+    phi2 = atan2(v1f * sin(theta1) + v2f * sin(theta2), v1f * cos(theta1) + v2f * cos(theta2))
+
+    return v1f, v2f, phi1, phi2
+
 #Particle Class
 class Particle:
     def __init__(self, x, y, is_tracer = False):
         self.x = x
         self.y = y
         self.radius = PARTICLE_RADIUS
+        
         self.color = RED
         self.speed = random.uniform(0, MAX_SPEED)
-        self.angle = random.uniform(0, 2*pi)
+        self.angle = random.uniform(-2*pi, 2*pi)
         self.is_tracer = is_tracer
         self.path = []
 
     def move(self):
         return
+
     
     def check_collision(self, other_particle):
         if sqrt((self.y - other_particle.y)**2 + (self.x - other_particle.x)**2) <= 2*PARTICLE_RADIUS:
             return True
+        else:
+            return False
         
     def check_wall_collision(self):
-        pass
+        if (self.x + self.radius) >= WIDTH or (self.x - self.radius) <= 0:
+            return True
+        else:
+            return False 
+        
+    def check_floor_roof_collision(self):
+        if (self.y + self.radius) >= HEIGHT or (self.y - self.radius) <= 0:
+            return True
+        else:
+            return False 
+        
 
     
 #Create particles
